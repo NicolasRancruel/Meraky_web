8528d43e356f9f0e3308a8946cad0c127868862c
Nicolas Rancruel <jm5281729@gmail.com@email.com>
2025-06-06 14:34:22 -0500

    Actualización: mejoras y nuevos cambios en el proyecto

--- js/main.js
+++ js/main.js
@@ -91,11 +91,5 @@ window.addEventListener("scroll", function () {
   }
 });
 
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.nav-links');
-
-hamburger.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
-});
 
 
